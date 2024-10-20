da40041f088416504c60e0cfb397ab8d78b2a0d9
jaygodara45 <102342813+jaygodara45@users.noreply.github.com>
2024-10-21 00:55:23 +0530

    Update setTokensCookies.js

--- utils/setTokensCookies.js
+++ utils/setTokensCookies.js
@@ -4,28 +4,28 @@ const setTokensCookies = (res, accessToken, refreshToken, newAccessTokenExp, new
 
   // Set Cookie for Access Token
   res.cookie('accessToken', accessToken, {
-    // httpOnly: true,
+    httpOnly: true,
     secure: true, // Set to true if using HTTPS
     maxAge: accessTokenMaxAge,
     sameSite: 'None',
-    Partitioned: true// Adjust according to your requirements
+    // Partitioned: true// Adjust according to your requirements
   });
 
   // Set Cookie for Refresh Token
   res.cookie('refreshToken', refreshToken, {
-    // httpOnly: true,
+    httpOnly: true,
     secure: true, // Set to true if using HTTPS
     maxAge: refreshTokenmaxAge,
     sameSite: 'None', 
-    Partitioned: true// Adjust according to your requirements
+    // Partitioned: true// Adjust according to your requirements
   });
   // Set Cookie for is_auth
   res.cookie('is_auth', true, {
-    // httpOnly: true,
+    httpOnly: true,
     secure: true, // Set to true if using HTTPS
     maxAge: refreshTokenmaxAge,
     sameSite: 'None', 
-    Partitioned: true// Adjust according to your requirements
+    // Partitioned: true// Adjust according to your requirements
   });
 }
 
